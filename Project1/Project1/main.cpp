9d16d131fce0b4a479fe67d08bafdfd126daff4b
Argha Biswas <argha.sust@gmail.com>
2026-04-06 12:28:17 +0600

    Fix Project1, remove DLLs

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main() {
+
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "My First Code");
+    window.setFramerateLimit(60);
+
+    sf::CircleShape circle(100.f);
+    circle.setFillColor(sf::Color::Red);
+
+    circle.setOrigin({ circle.getRadius(), circle.getRadius() });
+    circle.setPosition(sf::Vector2f(
+        window.getSize().x / 2.f,
+        window.getSize().y / 2.f
+    ));
+
+    int r = 150;
+    int g = 100;
+    int b = 150;
+
+    while (window.isOpen()) {
+
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+
+        window.clear(sf::Color(r, g, b));
+        window.draw(circle);
+        window.display();
+    }
+
+    return 0;
+}
+
+
+
---
+++ Project1/Project102/102.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Growing Circle");
+    window.setFramerateLimit(60);
+
+    sf::CircleShape circle(50.f);
+    circle.setFillColor(sf::Color::Red);
+
+    float radius = 50.f;
+    sf::Clock clock;
+
+    while (window.isOpen()) {
+
+        while (const std::optional event = window.pollEvent())
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        radius += 80.f * clock.restart().asSeconds();
+
+       
+        if (radius >= 300.f)
+            radius = 50.f;
+
+        circle.setRadius(radius);
+        circle.setOrigin({ radius, radius });
+        circle.setPosition({ 400.f, 300.f });
+
+        window.clear(sf::Color(150, 100, 150));
+        window.draw(circle);
+        window.display();
+    }
+}
